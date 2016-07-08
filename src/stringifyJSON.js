b27f0fc2d15e882ba29877825eb763e7537a07fd
Brandon Tiqui <btiqui@gmail.com>
2016-07-07 17:33:07 -0700

    Replace the stringifyJSON function and make the specs pass

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,29 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var types = ['number', 'boolean'];
+
+  if (types.indexOf(typeof obj) > -1 || obj === null) { return '' + obj; }
+  else if (typeof obj === 'string') { return '"' + obj + '"'; }
+
+  // Array
+  else if (Array.isArray(obj)) {
+	  var items = [];
+		for (var i = 0; i < obj.length; i++) {
+			items.push(stringifyJSON(obj[i]));
+		}
+		return '[' + items.join(',') + ']';
+  }
+
+  // Object
+  else if (typeof obj === 'object') {
+  	var properties = [];
+  	for (var key in obj) {
+  		var value = stringifyJSON(obj[key]);
+  		if (value !== undefined) {
+	  		properties.push(stringifyJSON(key) + ':' + value);
+	  	}
+  	}
+  	return '{' + properties.join(',') + '}';
+  }
 };
