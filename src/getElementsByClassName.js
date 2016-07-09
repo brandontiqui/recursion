891cc5adad0cb5e8b4e790e9d95c4bf3724dfaaf
Brandon Tiqui <btiqui@gmail.com>
2016-07-09 14:59:41 -0700

    Implement the getElementsByClassName function and make the specs pass

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,20 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  var elements = [];
+
+  var checkClass = function(element) {
+
+    // element.classList: whitespace won't have classList
+  	if (element.classList !== undefined && element.classList.contains(className)) {
+  		elements.push(element);
+  	}
+
+  	for (var i = 0; i < element.childNodes.length; i++) {
+  		checkClass(element.childNodes[i]);
+  	}
+  }
+
+  checkClass(document.body);
+  return elements;
 };
